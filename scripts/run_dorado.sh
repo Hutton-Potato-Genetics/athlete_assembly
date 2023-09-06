7ca7a410b93b597dbd52d41f983c37ff44f3cae1
SwiftSeal <65286772+SwiftSeal@users.noreply.github.com>
2023-09-06 10:11:53 +0100

    fix

--- scripts/run_dorado.sh
+++ scripts/run_dorado.sh
@@ -9,9 +9,13 @@
 #SBATCH -e slurm/dorado.%j.err
 
 dorado_path="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/bin/dorado"
-model="dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
+model="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/models/dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
 read_path="/mnt/shared/projects/jhi/potato/202302_dihaploid-ONT"
 
-$dorado_path duplex -r $model $read_path > $TMPDIR/duplex.fastq
+cp -r $read_path/legacy_pod5 $TMPDIR
+cp -r $read_path/dihaploid_2023-07-19 $TMPDIR
+cp -r $read_path/dihaploid_2023-07-21 $TMPDIR
+
+$dorado_path basecaller --emit-fastq -r $model $TMPDIR > $TMPDIR/duplex.fastq
 
 gzip -f $TMPDIR/duplex.fastq > duplex.fastq.gz
--- scripts/run_duplex.sh
+++ scripts/run_duplex.sh
@@ -9,9 +9,13 @@
 #SBATCH -e slurm/duplex.%j.err
 
 dorado_path="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/bin/dorado"
-model="dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
+model="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/models/dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
 read_path="/mnt/shared/projects/jhi/potato/202302_dihaploid-ONT"
 
-$dorado_path basecaller -r $model $read_path > $TMPDIR/reads.fastq
+cp -r $read_path/legacy_pod5 $TMPDIR
+cp -r $read_path/dihaploid_2023-07-19 $TMPDIR
+cp -r $read_path/dihaploid_2023-07-21 $TMPDIR
+
+$dorado_path duplex --emit-fastq -r $model $TMPDIR > $TMPDIR/reads.fastq
 
 gzip -f $TMPDIR/reads.fastq > reads.fastq.gz
