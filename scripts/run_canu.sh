c1b69bdc7d00f7394d883e76503058fb9919fa84
SwiftSeal <65286772+SwiftSeal@users.noreply.github.com>
2023-09-25 09:33:26 +0100

    fixes

--- scripts/run_canu.sh
+++ scripts/run_canu.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-#SBATCH -J canu
-#SBATCH -p short
-#SBATCH -c 1
-#SBATCH --mem=4G
-#SBATCH --export=ALL
-#SBATCH -o slurm/canu.%j.out
-#SBATCH -e slurm/canu.%j.err
-
 source activate vikrant_assembly
 
-canu -nanopore $TBC \
+canu -nanopore reads.fastq.gz \
 genomeSize=700m \
 gridOptions="--partition=long" \
-cnsMemory=10 -p dihaploid \
+cnsMemory=10 -p canu \
 -d canu
--- scripts/run_dorado.sh
+++ scripts/run_dorado.sh
@@ -9,13 +9,14 @@
 #SBATCH -e slurm/dorado.%j.err
 
 dorado_path="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/bin/dorado"
-model="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/models/dna_r10.4.1_e8.2_400bps_sup@v4.2.0"
+model="/mnt/shared/scratch/msmith/apps/dorado-0.3.4-linux-x64/models/dna_r10.4.1_e8.2_400bps_sup@v4.1.0"
 read_path="/mnt/shared/projects/jhi/potato/202302_dihaploid-ONT"
 
 cp -r $read_path/legacy_pod5 $TMPDIR
 cp -r $read_path/dihaploid_2023-07-19 $TMPDIR
 cp -r $read_path/dihaploid_2023-07-21 $TMPDIR
+cp -r $read_path/athlete $TMPDIR
 
-$dorado_path basecaller --emit-fastq -r $model $TMPDIR > $TMPDIR/duplex.fastq
+$dorado_path basecaller --emit-fastq -r $model $TMPDIR > $TMPDIR/reads.fastq
 
-gzip -f $TMPDIR/duplex.fastq > duplex.fastq.gz
+pigz -c reads.fastq > reads.fastq.gz
